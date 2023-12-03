4086ba0b5dd3c7edb018401c81bbaebcf2044b81
dima <madgohedkh@gmail.com>
2023-12-03 18:27:38 +0200

    download wp

--- wp-content/themes/calvin/comments.php
+++
@@ -1,3 +0,0 @@
-<div>
-  <?php wp_list_comments(); ?>
-</div>
--- wp-content/themes/calvin/templates/rubrics.php
+++
@@ -1,9 +0,0 @@
-<?php $links = array_map(function ($category) {
-                        return sprintf(
-                          '<a href="%s">%s</a>', // Шаблон вывода ссылки
-                          esc_url(get_category_link($category)), // Ссылка на рубрику
-                          esc_html($category->name) // Название рубрики
-                        );
-                      }, get_the_category());
-
-                      echo implode(' ', $links);
